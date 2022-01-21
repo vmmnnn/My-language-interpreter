143e6606fee58dad940df1725364d040945db4df
Marina <vmmnnn@mail.ru>
2022-01-21 23:36:10 +0300

    interpreter separated

--- src/main/scala/my/Interpreter.scala
+++ src/main/scala/my/Interpreter.scala
@@ -3,16 +3,25 @@ package my
 import scala.io.Source.fromFile
 
 
-object Interpreter extends Serializable {
+object Main extends Serializable {
 	def main(args: Array[String]): Unit = {
 		var fileName = "examples/program.txt"
 		args.sliding(2, 2).collect {
 			case Array("--fileName", value) => fileName = value
 		}
 		val program = fromFile(fileName).mkString
+
+		val interpreter = new Interpreter(program)
+		interpreter.run()
+	}
+}
+
+class Interpreter(val program: String) {
+	def run(): Unit = {
 		val lexicalAnalyzer = new LexicalAnalyzer
 		lexicalAnalyzer.run(program)
 
-		lexicalAnalyzer.lexemesTable.print()
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		lexemesParser.run()
 	}
 }
