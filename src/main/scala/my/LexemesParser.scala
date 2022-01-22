5ec6ec188c9d07cd173c075688654bc1840293b4
Marina <vmmnnn@mail.ru>
2022-01-22 12:58:54 +0300

    non-main function are skipped

--- src/main/scala/my/LexemesParser.scala
+++ src/main/scala/my/LexemesParser.scala
@@ -1,6 +1,7 @@
 package my
 
-import my.LexemeType.{BoolVal, DoubleNumber, IntNumber}
+import com.sun.org.apache.bcel.internal.classfile.LineNumber
+import my.LexemeType.{BoolVal, Brackets, Colon, Comma, DoubleNumber, IntNumber, Name, Type}
 
 import scala.collection.mutable.Map
 
@@ -48,21 +49,35 @@ class VarTable {
 }
 
 /**
- * Parse lexemes given and runs the program
+ * Parses lexemes given and runs the program
  * @param lexemeTable result of lexical analysis
+ * @param mainFunction function to be run, usually 'main'
  */
-class LexemesParser(lexemeTable: LexemeTable) {
+class LexemesParser(lexemeTable: LexemeTable, mainFunction: String = "main") {
+	// current lexeme, updates during parsing
 	private var lexeme = lexemeTable.next()
+
+	// table with global variables
 	private val globalVars = new VarTable
 
+	// table of functions: name, 'def'-lexeme position in LexemeTable
+	private val functionTable: Map[String, Int] = Map.empty
+
 	def getGlobalVars: VarTable = globalVars
+	def getFunctionTable: Map[String, Int] = functionTable
 
 	def run(): Unit = {
 		// program starts either from global variables or right from functions
 		while (lexeme.isDefined) {
-			if (lexeme.get.lexemeType == LexemeType.Name) {  // global variables
+			if (lexeme.get.lexemeType == LexemeType.Name) { // global variables
 				val (name, value) = getVariable()
 				globalVars.setVal(name, value)
+			} else {
+				if (lexeme.get.value == "def") { // function
+					parseFunction()
+				} else { // something else => error
+					sendUnexpectedTokenError()
+				}
 			}
 		}
 	}
@@ -74,6 +89,30 @@ class LexemesParser(lexemeTable: LexemeTable) {
 	private def sendError(message: String, lineNumber: Int): Unit = {
 		System.err.println(f"line ${lineNumber}: $message")
 		throw new Exception
+	}
+
+	/**
+	 * Sends "Unexpected token '...'" message with current lexeme
+	 */
+	private def sendUnexpectedTokenError(): Unit = {
+		sendError(f"Unexpected token ${lexeme.get.value}", lexeme.get.lineNumber)
+	}
+
+	/**
+	 * Sends "Unexpected token '...'" message with token and position given
+	 * @param token wrong lexeme found
+	 * @param lineNumber position
+	 */
+	private def sendUnexpectedTokenError(token: String, lineNumber: Int): Unit = {
+		sendError(f"Unexpected token $token", lineNumber)
+	}
+
+	/**
+	 * Sends "'...' expected, but '...' found" message
+	 * @param expected what was expected
+	 */
+	private def sendExpectedFoundError(expected: String): Unit = {
+		sendError(f"$expected expected, but ${lexeme.get.value} found", lexeme.get.lineNumber)
 	}
 
 	/**
@@ -128,21 +167,19 @@ class LexemesParser(lexemeTable: LexemeTable) {
 
 		def getValueCheckType(): Unit = {
 			varValue = Option(lexeme.get.value)
-			val lineNumber = lexeme.get.lineNumber
 			lexeme.get.lexemeType match {
 				case BoolVal if varType.get == VarType.Bool =>
 				case DoubleNumber if varType.get == VarType.Double =>
 				case IntNumber if varType.get == VarType.Int =>
 				case IntNumber if varType.get == VarType.Double =>
 				case LexemeType.String if varType.get == VarType.String =>
-				case _ =>
-					sendError(f"Types do not match: ${varType.get} expected, but ${lexeme.get.lexemeType} found", lineNumber)
+				case _ => sendExpectedFoundError(f"Types do not match: ${varType.get}")
 			}
 		}
 
 		def getValueSetType(): Unit = {
 			if (!isValue(lexeme.get)) {
-				sendError(f"Unexpected symbol ${lexeme.get.value}", lexeme.get.lineNumber)
+				sendUnexpectedTokenError()
 			}
 			varValue = Option(lexeme.get.value)
 			varType = Option(lexeme.get.lexemeType match {
@@ -165,14 +202,14 @@ class LexemesParser(lexemeTable: LexemeTable) {
 
 		if (lexeme.isEmpty) {
 			if (varType.isEmpty) {
-				sendError(f"Unknown token '${varName}'", lineNumber)
+				sendUnexpectedTokenError(varName, lineNumber)
 			}
 			return (varName, new Value(varType.get, None))
 		}
 
 		// Neither colon for type nor '=' for value were found
 		if (varType.isEmpty & (lexeme.get.lexemeType != LexemeType.DefineOp)) {
-			sendError(f"Unknown token '${varName}'", lineNumber)
+			sendUnexpectedTokenError(varName, lineNumber)
 		}
 
 		// '=' => look for a value
@@ -187,5 +224,117 @@ class LexemesParser(lexemeTable: LexemeTable) {
 
 		lexeme = lexemeTable.next()
 		(varName, new Value(varType.get, varValue))
+	}
+
+	/**
+	 * Called when 'def' key word found
+	 * If it is 'main' function - run it
+	 * Otherwise save it in function table to call it if needed
+	 */
+	private def parseFunction(): Unit = {
+		nextLexemeCheckEmpty()  // should be function name
+		if (lexeme.get.lexemeType != Name) {
+			sendUnexpectedTokenError()
+		}
+		val name = lexeme.get.value
+		if (name == mainFunction) {
+			runMain()
+		} else {
+			val checkIfExists = functionTable.get(name)
+			if (checkIfExists.isDefined) {  // the function has been already defined
+				val errorMessage = f"Duplicated function name $name."
+				val previousDefinitionMessage = f"Previous declaration in line $checkIfExists"
+				sendError(f"$errorMessage\n$previousDefinitionMessage", lexeme.get.lineNumber)
+			}
+			functionTable(name) = lexeme.get.lineNumber
+			skipFunction()
+		}
+	}
+
+	private def runMain(): Unit = {}
+
+	/**
+	 * Skips lexemes until function ends (def name(...): Type {...})
+	 * Should be called when lexeme is a function name
+ 	 */
+	private def skipFunction(): Unit = {
+		def functionParameters(): Unit = {
+			while (lexeme.get.lexemeType == Name) { // function parameters
+				nextLexemeCheckEmpty()
+				if (lexeme.get.lexemeType != Colon) {
+					sendExpectedFoundError("Type")
+				}
+				nextLexemeCheckEmpty()
+				if (lexeme.get.lexemeType != Type) {
+					sendExpectedFoundError("Type")
+				}
+				nextLexemeCheckEmpty() // either ',' or ')'
+				if (lexeme.get.lexemeType == Comma) {
+					nextLexemeCheckEmpty()
+					if (lexeme.get.lexemeType != Name) {
+						sendExpectedFoundError("Next parameter after ','")
+					}
+				}
+			}
+		}
+
+		// def name(...): Type {
+		def header(): Unit = {
+			nextLexemeCheckEmpty()
+			if (lexeme.get.lexemeType != Brackets | lexeme.get.value != "(") {
+				sendExpectedFoundError("Function parameters in brackets")
+			}
+			nextLexemeCheckEmpty()
+			functionParameters()
+			if (lexeme.get.value != ")") {
+				sendUnexpectedTokenError()
+			}
+			nextLexemeCheckEmpty()
+			if (lexeme.get.lexemeType != Colon) {
+				sendExpectedFoundError("Function type")
+			}
+			nextLexemeCheckEmpty()
+			if (lexeme.get.lexemeType != Type) {
+				sendExpectedFoundError("Function type")
+			}
+			nextLexemeCheckEmpty() // function code starts: '{' expected
+			if (lexeme.get.value != "{") {
+				sendExpectedFoundError("'{'")
+			}
+		}
+
+		def isPairedBrackets(open: String, close: String): Boolean = {
+			open match {
+				case "(" if close == ")" => true
+				case "{" if close == "}" => true
+				case "[" if close == "]" => true
+				case _ => false
+			}
+		}
+
+		def bracketsBalance(): Unit = {
+			var bracketsStack: List[String] = List.empty
+			val openBrackets = Array("(", "{", "[")
+			val closeBrackets = Array(")", "}", "]")
+			bracketsStack = "{" :: bracketsStack
+
+			while (!bracketsStack.isEmpty) {
+				nextLexemeCheckEmpty()
+				val value = lexeme.get.value
+				if (openBrackets.contains(value)) {
+					bracketsStack = value :: bracketsStack
+				} else if (closeBrackets.contains(value)) {
+					val stackTop = bracketsStack.head
+					if (!isPairedBrackets(stackTop, value)) {
+						sendError("Mismatched parentheses", lexeme.get.lineNumber)
+					}
+					bracketsStack = bracketsStack.tail
+				}
+			}
+		}
+
+		header()
+		bracketsBalance()
+		lexeme = lexemeTable.next()
 	}
 }
--- src/test/scala/my/LexemesParserSuite.scala
+++ src/test/scala/my/LexemesParserSuite.scala
@@ -2,6 +2,8 @@ package my
 
 import org.scalatest.FunSuite
 
+import scala.collection.mutable.Map
+
 
 class LexemesParserSuite extends FunSuite {
 	def sameVarTables(table1: VarTable, table2: VarTable): Boolean = {
@@ -12,6 +14,18 @@ class LexemesParserSuite extends FunSuite {
 			val variable1 = table1.getVal(name).get
 			val variable2 = table2.getVal(name).get
 			if (!variable1.isSame(variable2)) return false
+		}
+		true
+	}
+
+	def sameFunctionTables(table1: Map[String, Int], table2: Map[String, Int]): Boolean = {
+		val keys1 = table1.keys.toArray
+		val keys2 = table2.keys.toArray
+		if (!keys1.sameElements(keys2)) return false
+		keys1.foreach{ case name =>
+			val val1 = table1(name)
+			val val2 = table2(name)
+			if (val1 != val2) return false
 		}
 		true
 	}
@@ -145,6 +159,183 @@ class LexemesParserSuite extends FunSuite {
 		globalVars.print()
 
 		assert(sameVarTables(expected, globalVars))
+	}
+
+	test("def func1(): None {}") {
+		val lexemeTable: LexemeTable = new LexemeTable()
+			.add(new Lexeme("def", LexemeType.KeyWord, 1))
+			.add(new Lexeme("func1", LexemeType.Name, 1))
+			.add(new Lexeme("(", LexemeType.Brackets, 1))
+			.add(new Lexeme(")", LexemeType.Brackets, 1))
+			.add(new Lexeme(":", LexemeType.Colon, 1))
+			.add(new Lexeme("None", LexemeType.Type, 1))
+			.add(new Lexeme("{", LexemeType.Brackets, 1))
+			.add(new Lexeme("func2", LexemeType.Name, 2))
+			.add(new Lexeme("(", LexemeType.Brackets, 2))
+			.add(new Lexeme(")", LexemeType.Brackets, 2))
+			.add(new Lexeme("}", LexemeType.Brackets, 3))
+			.add(new Lexeme("x", LexemeType.Name, 4))
+			.add(new Lexeme("=", LexemeType.DefineOp, 4))
+			.add(new Lexeme("3", LexemeType.IntNumber, 4))
+
+		val lexemesParser = new LexemesParser(lexemeTable)
+		lexemesParser.run()
+
+		val globalVars = lexemesParser.getGlobalVars
+		val functionTable = lexemesParser.getFunctionTable
+
+		val expectedGlobalVars: VarTable = new VarTable
+		val expectedVar: Value = new Value(VarType.Int, Option("3"))
+		expectedGlobalVars.setVal("x", expectedVar)
+
+		val expectedFunctionTable: Map[String, Int] = Map("func1" -> 1)
+
+		assert(sameVarTables(expectedGlobalVars, globalVars))
+		assert(sameFunctionTables(expectedFunctionTable, functionTable))
+	}
+
+	test("2 functions with parameters") {
+		val program = "def func1(p1: Int, p2: Double): Bool {\n" +
+			"x = (2+2)/2\n" +
+			"if (func2(p1) == 1) {return False}\n" +
+			"return True\n" +
+			"}\n" +
+			"p = 0\n" +
+			"def func2(p3: Int): Double {return (p+p3)/p3}\n"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		lexemesParser.run()
+
+		val globalVars = lexemesParser.getGlobalVars
+		val functionTable = lexemesParser.getFunctionTable
+
+		val expectedGlobalVars: VarTable = new VarTable
+		val expectedVar: Value = new Value(VarType.Int, Option("0"))
+		expectedGlobalVars.setVal("p", expectedVar)
+
+		val expectedFunctionTable: Map[String, Int] = Map("func1" -> 1, "func2" -> 7)
+
+		assert(sameVarTables(expectedGlobalVars, globalVars))
+		assert(sameFunctionTables(expectedFunctionTable, functionTable))
+	}
+
+	test("Function type missed") {
+		val program = "def f(): {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Colon missed") {
+		val program = "def f() Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("'def' missed") {
+		val program = "f(): Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Function name missed") {
+		val program = "def (): Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Parameter open bracket missed") {
+		val program = "def f): Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Parameter close bracket missed") {
+		val program = "def f(: Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Parameter brackets missed") {
+		val program = "def f: Int {}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Parameter code open brackets missed") {
+		val program = "def f(): Int x=3}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Parameter code close brackets missed") {
+		val program = "def f(): Int {x=3"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Brackets mismatched: no ')'") {
+		val program = "def f(): Int {x=(3}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Brackets mismatched: no '('") {
+		val program = "def f(): Int {x=3)}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Brackets mismatched: no '}'") {
+		val program = "def f(): Int {x={3}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
+	}
+
+	test("Brackets mismatched: no '{'") {
+		val program = "def f(): Int {x=3}}"
+		val lexicalAnalyzer = new LexicalAnalyzer
+		lexicalAnalyzer.run(program)
+
+		val lexemesParser = new LexemesParser(lexicalAnalyzer.lexemesTable)
+		intercept[Exception] { lexemesParser.run() }
 	}
 
 }
